662d4eea070425469a6d40e9208761e79672802e
Merran20 <meruzhangevorgin@hotmail.com>
2020-10-23 13:16:41 +0200

    inne yao yao

---
+++ Logic/Entities/Person Entities/Mekanik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic.Entities.Person_Entities
+{
+    public class Mekanik
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateOfBirthday { get; set; }
+
+        public DateTime dateOfEnd { get; set; }
+
+        public List<string> Skills { get; set; }
+
+        public List<User> users { get; set; }
+    }
+}
