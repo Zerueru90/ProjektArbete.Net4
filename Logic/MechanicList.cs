0aa482a3e3d8444aee9e3102cf4bcd3a94a3448d
Samson <31880780+Zerueru90@users.noreply.github.com>
2020-10-26 12:03:55 +0100

    Skapat en mekanikerlista

---
+++ Logic/MechanicList.cs
@@ -0,0 +1,30 @@
+﻿using Logic.Entities.Person_Entities;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Logic
+{
+    public static class MechanicList
+    {
+        private static List<Mechanic> _mechanics;
+
+        public static List<Mechanic> AddToMechanicList
+        {
+            get
+            {
+                if (_mechanics == null)
+                {
+                    return _mechanics = new List<Mechanic>();
+                }
+                return _mechanics;
+            }
+            set
+            {
+                _mechanics = value;
+            }
+        }
+
+    }
+}
