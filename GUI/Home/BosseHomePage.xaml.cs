13e793acb68497792c6a4dc7f7f03c5ba43d28de
Samson <31880780+Zerueru90@users.noreply.github.com>
2020-10-28 13:08:17 +0100

    Ändringar

--- GUI/Home/BosseHomePage.xaml.cs
+++ GUI/Home/BosseHomePage.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,16 +28,16 @@ namespace GUI.Home
         {
             InitializeComponent();
 
-            List<Mechanic> mechanics = new List<Mechanic>();
-
             //Hämtar från Listan och lägger upp allt i DataGrid, längre fram så kommer det innehålla mekaniker i listan från Json filen.
             foreach (var item in MechanicList._mechanicList)
             {
-                mechanics.Add(new Mechanic() { Id = item.Id, Name = item.Name, DateOfBirthday = item.DateOfBirthday, DateOfEmployment = item.DateOfEmployment, DateOfEnd = item.DateOfEnd, SkillLista = item.SkillLista, users = item.users });
+                MechanicList._mechanicList.Add(new Mechanic() { Id = item.Id, Name = item.Name, DateOfBirthday = item.DateOfBirthday, DateOfEmployment = item.DateOfEmployment, DateOfEnd = item.DateOfEnd, SkillLista = item.SkillLista, users = item.users });
             }
+            //Dessa två är för att fylla vår datagrid/lista av mekaniker från listan.
+            dgUserAccess.ItemsSource = MechanicList._mechanicList;
+            dgMainPage.ItemsSource = MechanicList._mechanicList;
 
-            dgUserAccess.ItemsSource = mechanics;
-            dgMainPage.ItemsSource = mechanics;
+            MechanicList._mechanicList.Add(new Mechanic() { Name = "John", DateOfBirthday = DateTime.Now, DateOfEmployment = DateTime.Now, DateOfEnd = DateTime.Now, SkillLista = { "Bromsar", "Kaross", "Vindruta" } });
 
             txtName.Text = "Lasse";
             txtEmployementday.Text = "20-10-24";
@@ -62,35 +63,11 @@ namespace GUI.Home
             }
         }
 
-        //Vad denna metod ska göras är när mekaniker hämtas från listan så ska checkboxen checkas men kommer inte på vad för kod man skriver för att manuellt klicka i checkbox.
-        private void SkillCheck2(string skill)
-        {
-            if (skill == _breakes)
-            {
-                //Om strängen existerar så checkas boxen.
-            }
-            if (skill == _engine)
-            {
-                //Om strängen existerar så checkas boxen.
-            }
-            if (skill == _carbody)
-            {
-                //Om strängen existerar så checkas boxen.
-            }
-            if (skill == _windshield)
-            {
-                //Om strängen existerar så checkas boxen.
-            }
-            if (skill == _tyre)
-            {
-                //Om strängen existerar så checkas boxen.
-            }
-        }
-
         private void BtnSaveNewMechanic(object sender, RoutedEventArgs e)
-        {
+        { 
             if (!(txtName.Text == null) || !(txtName.Text == ""))
             {
+                
                 _mechanic = new Mechanic
                 {
                     Name = txtName.Text,
@@ -119,23 +96,9 @@ namespace GUI.Home
             }
         }
 
-        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            if (dgMainPage.SelectedItem != null)
-            {
-                //Här så ska man välje en mekaniker från lista, kunna ändra namn osv och sen uppdatera.
-            }
-        }
-
-
         private void BtnNewUser_Click(object sender, RoutedEventArgs e)
         {
-            _newUser = new User()
-            {
-                Username = txtUserName.Text,
-                Password = txtPassword.Text
-            };
-            _crud.AddUser(_newUser);
+            _crud.AddUser(_newUser = new User() { Username = txtUserName.Text, Password = txtPassword.Text });
         }
 
         private void BtnDeleteUser_Click(object sender, RoutedEventArgs e)
